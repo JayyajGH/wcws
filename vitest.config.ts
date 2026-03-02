9e3530054637eb7f01759ef7db160955346ee79a
Jason Harris <jayyaj@hotmail.com>
2026-03-02 21:45:45 +0000

    Use threads rather than forks when runing vitest to apease antigravity

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    pool: 'threads',
     setupFiles: './vitest.setup.ts',
     coverage: {
       provider: 'v8', 
